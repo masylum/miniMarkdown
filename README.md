33006dfa365dcb660ab8d7fd577c02659d598406
Pau Ramon Revilla <masylum@gmail.com>
2015-09-28 18:33:50 +0200

    First commit :eggplant:

---
+++ README.md
@@ -0,0 +1,54 @@
+# miniMarkdown
+
+![](https://media.giphy.com/media/qs6ev2pm8g9dS/giphy.gif)
+
+^ Markdown
+
+An easy to extend parser for a subset of markdown.
+
+## Markdown support
+
+miniMarkdown supports:
+
+  - ```` ```pre``` ````
+  - `` `code` ``
+  - ` _Italics_ `
+  - ` *Bold* `
+  - ` ~Strike~ `
+
+You can easily add more grammar to make your own extensions
+
+## API
+
+To run with the default grammar:
+
+```js
+let miniMarkdown = require('miniMarkdown');
+let text = 'this *is* an _example_';
+let grammar = miniMarkdown.DEFAULT_GRAMMAR;
+let tokenizer = new miniMarkdown.Tokenizer(text, grammar);
+
+tokenizer.run();
+=> [
+     {type: 'word', text: 'this '}
+   , {type: 'bold', children: [{type: 'word', text: 'is'}]}
+   , {type: 'word', text: ' an '}
+   , {type: 'italic', children: [{type: 'word', text: 'example'}]}
+   ]
+```
+
+If you are interested into adding more grammar you can also do so!
+
+`TokenType` offers you an API to define grammar:
+
+  - `start`: The string that defines the beggining of a token.
+  - `end`: The string that defines the ending of a token.
+  - `surround`: When both start and end delimiters are the same you can use this instead.
+  - `grammar`: If you want a different grammar for a given token.
+  - `constraint`: If you want to validate whether the token is valid before pushing it to the tree.
+
+You can see grammar examples on the `spec/grammar_spec.js` test file.
+
+## TEST
+
+To test just run `npm test`
---
+++ miniMarkdown.js
@@ -0,0 +1,226 @@
+(function (root, factory) {
+  if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.miniMarkdown = factory();
+  }
+}(this, function () {
+
+  // ------------------------
+  // -- Token
+  // ------------------------
+
+  /**
+   * Struct for a given token
+   */
+  function Token(type, children, text) {
+    this.type = type;
+
+    if (children) {
+      this.children = children;
+    }
+
+    if (text) {
+      this.text = text;
+    }
+  }
+
+  // ------------------------
+  // -- TokenType
+  // ------------------------
+
+  /**
+   * Struct to define a token type
+   * This allows the tokenizer to match the string or not
+   *
+   * @param {String} type
+   * @param {Object} options
+   *   - {Function} chunker
+   */
+  function TokenType(type, options) {
+    this.type = type;
+
+    if (options.grammar) {
+      this.grammar = [this].concat(options.grammar);
+    }
+
+    this.constraint = options.constraint;
+
+    if (options.surround) {
+      this.start = this.end = options.surround;
+    } else {
+      this.start = options.start;
+      this.end = options.end;
+    }
+
+    this.leaf = !this.start && !this.end;
+  }
+
+  /**
+   * Is the following chunk a token of my type?
+   *
+   * @param {TokenType} contextType
+   * @param {String} text
+   * @param {Boolean}
+   */
+  TokenType.prototype.match = function (contextType, text) {
+    if (this.leaf) return this.constraint ? this.constraint(text) : true;
+
+    var start, end;
+
+    if (contextType === this && this.end) {
+      end = text.slice(0, this.end.length);
+      return end === this.end;
+    }
+
+    if (!this.start) {
+      return true;
+    }
+
+    start = text.slice(0, this.start.length);
+    return start === this.start;
+  };
+
+  // ------------------------
+  // -- DEFAULT_GRAMMAR
+  // ------------------------
+
+  var word = new TokenType('word', {});
+
+  var DEFAULT_GRAMMAR = [
+    new TokenType('pre',     {surround: '```', grammar: []})
+  , new TokenType('code',    {surround: '`'})
+  , new TokenType('italic',  {surround: '_'})
+  , new TokenType('bold',    {surround: '*'})
+  , new TokenType('strike',  {surround: '~'})
+  ];
+
+  // ------------------------
+  // -- Tokenizer
+  // ------------------------
+
+  /**
+   * This is the responsible for consuming
+   * the text and generate an array of tokens
+   *
+   * @param {String} text
+   * @param {Array<TokenType} grammar
+   */
+  function Tokenizer(text, grammar, contextType) {
+    this.text = text;
+    this.grammar = grammar.concat([word]);
+
+    this.i = 0;
+    this.start = 0;
+    this.tokens = [];
+    this.currentType = null;
+    this.contextType = contextType || null;
+    this.chunk = this.text;
+  }
+
+  Tokenizer.prototype.run = function () {
+    do {
+      var tuple = this.fetchNextToken();
+      var length = tuple[0];
+      var tokenType = tuple[1];
+
+      if (tokenType === this.currentType) continue;
+      if (tokenType === this.contextType) return this.finishIncompleteLeaf();
+
+      if (tokenType.leaf) {
+        this.finishIncompleteLeaf();
+        this.start = this.i;
+        this.currentType = tokenType;
+
+        if (length > 1) {
+          this.i += length;
+          this.finishIncompleteLeaf();
+          this.i -= 1;
+          this.currentType = null;
+        }
+      } else {
+        this.pushBranchToken(tokenType);
+      }
+    } while (this.next());
+
+    if (this.contextType) return [];
+
+    return this.finishIncompleteLeaf();
+  };
+
+  Tokenizer.prototype.finishIncompleteLeaf = function () {
+    if (this.currentType) this.pushLeafToken();
+
+    return this.tokens;
+  };
+
+  Tokenizer.prototype.pushLeafToken = function () {
+    var text = this.text.slice(this.start, this.i);
+
+    this.tokens.push(new Token(this.currentType.type, null, text));
+  }
+
+  /**
+   * Starts a new Tokenizer to evaluate the branch
+   * and pushes both the leaf (if was open) and the
+   * closed branch
+   *
+   * @param {TokenType} tokenType
+   */
+  Tokenizer.prototype.pushBranchToken = function (tokenType) {
+    var nextChunk = this.chunk.slice(tokenType.start ? tokenType.start.length : 0)
+      , tokenizer
+      , children;
+
+    if (!nextChunk.length) return;
+
+    tokenizer = new Tokenizer(nextChunk, tokenType.grammar || this.grammar, tokenType);
+    children = tokenizer.run();
+
+    if (!children.length) return;
+    if (this.currentType) this.pushLeafToken();
+
+    this.i += tokenType.start.length + tokenizer.i + tokenType.end.length - 1;
+    this.tokens.push(new Token(tokenType.type, children, null));
+    this.currentType = null;
+  };
+
+  /**
+   * Set the chuck to be evaluated
+   *
+   * @return {Boolean}
+   */
+  Tokenizer.prototype.next = function () {
+    this.i++;
+    this.chunk = this.text.slice(this.i);
+
+    return !!this.chunk.length;
+  };
+
+  /**
+   * Iterates all the token types
+   * until finding one that matches
+   *
+   * @return {Array<Number, TokenType>}
+   */
+  Tokenizer.prototype.fetchNextToken = function () {
+    var length = this.grammar.length
+      , i = 0
+      , match
+      , token_type;
+
+    for (i = 0; i < length; i++) {
+      token_type = this.grammar[i];
+      match = token_type.match(this.contextType, this.chunk)
+      if (match) {
+        return [match, token_type];
+      }
+    }
+  };
+
+  return {
+    Tokenizer: Tokenizer
+  , DEFAULT_GRAMMAR: DEFAULT_GRAMMAR
+  , TokenType: TokenType
+  }
+}));
